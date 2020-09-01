0e7b50d19a91170ab96f681b70e1969f6e62457c
HBENMOHAND <hedi.benmohand@ynov.com>
2020-09-01 12:27:19 +0200

    exo2

---
+++ maxOfThree.py
@@ -0,0 +1,8 @@
+def maxofthree(arg1, arg2, arg3):
+    if arg1 > arg2 and arg1 > arg3:
+        print(arg1)
+    elif arg2 > arg1 and arg2 > arg3:
+        print(arg2)
+    else:
+        print(arg3)
+maxofthree(4,2,3)
